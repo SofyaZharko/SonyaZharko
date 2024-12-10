86f64eacb72f3209b748484a1ddcaab6cb2550ce
Sonya Zharko <Zharko26101982@yandex.ru>
2024-12-10 17:36:20 +0500

    hw

---
+++ lesson_12.py
@@ -0,0 +1,47 @@
+import pandas as pd # type: ignore
+import matplotlib.pyplot as plt # type: ignore
+import seaborn as sns # type: ignore
+
+file_path = '/mnt/data/Mall_Customers.csv'
+data = pd.read_csv(file_path)
+data.head(), data.info()
+
+
+
+
+
+
+average_income_women = data[data['Genre'] == 'Female']['Annual Income (k$)'].mean()
+
+
+
+
+
+highest_spender_male = data[data['Genre'] == 'Male'].sort_values(by='Spending Score (1-100)', ascending=False).iloc[0]
+highest_spender_female = data[data['Genre'] == 'Female'].sort_values(by='Spending Score (1-100)', ascending=False).iloc[0]
+
+
+
+
+
+male_data = data[data['Genre'] == 'Male']
+plt.figure(figsize=(10, 6))
+sns.lineplot(x='Age', y='Annual Income (k$)', data=male_data, marker='o', color='blue')
+plt.title('Зависимость доходов от возраста (мужчины)')
+plt.xlabel('Возраст')
+plt.ylabel('Годовой доход (k$)')
+plt.grid(True)
+plt.show()
+
+
+
+
+
+plt.figure(figsize=(12, 8))
+sns.histplot(data=data, x='Annual Income (k$)', hue='Genre', weights='Spending Score (1-100)', multiple='dodge', kde=False, palette={'Male': 'blue', 'Female': 'pink'}, bins=15)
+plt.title('Распределение расходов в зависимости от доходов')
+plt.xlabel('Годовой доход (k$)')
+plt.ylabel('Общие расходы (оценка)')
+plt.grid(axis='y')
+plt.show()
+print(average_income_women, highest_spender_male, highest_spender_female)
